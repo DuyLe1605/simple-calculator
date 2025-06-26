26aa0cc945bf76ef5899e2c64648994213c450df
DuyLe1605 <leminhduy160505@gmail.com>
2025-06-26 09:33:54 +0700

    Khởi tạo dự án

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    experimental: {
+        reactCompiler: true,
+    },
+    eslint: {
+        dirs: ["pages", "utils"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+        ignoreDuringBuilds: true, // ✅ bỏ qua lỗi ESLint khi build, nên xóa khi deploy thật
+    },
 };
 
 export default nextConfig;
